c22b47113616be40eb79e0ad1bc96f58faf24f86
sandeep yadav <sandeepyada234abc@gmail.com>
2025-10-31 07:12:43 +0530

    add cookie

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ app.use(express.urlencoded({extended : true}));
 let sessionOptions = ({
     secret: "itismysupersecretkey",
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
