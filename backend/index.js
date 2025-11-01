07f13692336ee071e729857001c873d70a49eef0
sandeep yadav <sandeepyada234abc@gmail.com>
2025-11-01 07:32:02 +0530

    add cookie

--- backend/index.js
+++ backend/index.js
@@ -55,10 +55,9 @@ let sessionOptions = ({
     secret: "itismysupersecretkey",
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({ mongoUrl: process.env.MONGO_URL }),
     cookie: {
-        secure: true,
         sameSite: "none",
+        secure: true,
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
